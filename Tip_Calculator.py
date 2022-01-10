00ef61ab6350b114e090e19e35e74adab4503ae6
Parth4786 <80087434+Parth4786@users.noreply.github.com>
2022-01-10 16:00:24 +0530

    Delete Tip_Calculator.py
    Don't want clutter will add projects to respective day only.

--- Tip_Calculator.py
+++
@@ -1,8 +0,0 @@
-print('Welcome to the tip calculator!')
-bill_amount = float(input('What was the total bill?\nRs '))
-No_of_persons =int(input('How many people are to split the bill?\n'))
-tip_percentage =int(input('What percentage of tip would you like to give?\n'))
-tip_amount = ((bill_amount/100)*tip_percentage)
-total_amount = bill_amount + tip_amount
-individual_amount = total_amount/No_of_persons
-print(f"Each person has to pay: Rs {round(individual_amount,2)}")
